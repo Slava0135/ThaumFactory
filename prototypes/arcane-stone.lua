bf0de46b076f6e8dc01298612e657a31a88c8855
slava0135 <super.novalskiy_0135@inbox.ru>
2025-08-19 21:16:44 +0300

    feat: tint arcane stone brick texture

--- prototypes/arcane-stone.lua
+++ prototypes/arcane-stone.lua
@@ -5,6 +5,12 @@ data:extend({
     type = "item",
     name = "thaumfactory-arcane-stone-brick",
     icon = "__base__/graphics/icons/stone-brick.png",
+    icons = {
+      {
+        icon = "__base__/graphics/icons/stone-brick.png",
+        tint = { 95, 95, 95 } -- #5F5F5F
+      }
+    },
     subgroup = "thaumfactory-components",
     order = "t-arcane-stone-brick",
     inventory_move_sound = item_sounds.brick_inventory_move,
