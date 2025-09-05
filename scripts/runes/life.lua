329d43feb75bbce03923c6074dc4a645f2f7ca3a
slava0135 <super.novalskiy_0135@inbox.ru>
2025-09-05 09:46:20 +0300

    feat: increase life rune efficiency

--- scripts/runes/life.lua
+++ scripts/runes/life.lua
@@ -12,14 +12,24 @@ local function on_12th_tick()
     if player.controller_type == defines.controllers.character then
       local rune = player.get_main_inventory().find_item_stack("thaumfactory-life-rune-stone")
       if rune and rune.durability > 1 and player.character and player.character.get_health_ratio() < 1 then
-        player.character.damage(-1, "player")
-        rune.drain_durability(1)
+        local heal = 1
+        player.character.damage(-heal, "player")
+        storage.life_healed[player.index] = (storage.life_healed[player.index] or 0) + heal
+        -- 1 durability per 12 hp
+        if storage.life_healed[player.index] >= 12 then
+          storage.life_healed[player.index] = 0
+          rune.drain_durability(1)
+        end
       end
     end
   end
 end
 
 local life_rune = {}
+
+life_rune.on_init = function()
+  storage.life_healed = {}
+end
 
 life_rune.events = {
   [restore_handler_id] = restore_handler
